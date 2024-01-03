4f6e8433d7b234101f4264579e1512be87abcf79
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-03 22:42:59 +0530

    image upload preview

--- src/Components/PostTweet.js
+++ src/Components/PostTweet.js
@@ -3,24 +3,36 @@ import React, { useState } from "react";
 import { auth, db, storage } from "../firebase";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import BrokenImageOutlinedIcon from "@mui/icons-material/BrokenImageOutlined";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostTweet = () => {
   const [title, setTitle] = useState("");
-  const [file, setFile] = useState(null);
+  const [uploadedImageURL, setUploadedImageURL] = useState(null);
+  const [isImageUploading, setIsImageUploading] = useState(false);
 
-  const handlePost = async () => {
+  const handleImageUpload = async (e) => {
+    setIsImageUploading(true);
+    const file = e.target.files[0];
     const fileName = `${Date.now()}-${file.name}`;
     const storageRef = ref(storage, fileName);
     const snapshot = await uploadBytes(storageRef, file);
     const downloadURL = await getDownloadURL(snapshot.ref);
+    setUploadedImageURL(downloadURL);
+    setIsImageUploading(false);
+  };
+
+  const handlePost = async () => {
     const docRef = await addDoc(collection(db, "posts"), {
       title: title,
-      imageURL: downloadURL,
+      imageURL: uploadedImageURL,
       createdAt: new Date(),
       authorUID: auth.currentUser.uid,
       authorPhotoURL: auth.currentUser.photoURL,
       authorName: auth.currentUser.displayName,
     });
+    setTitle("");
+    setUploadedImageURL(null);
   };
   return (
     <div className="flex gap-2 p-3 border-b border-gray-800 mt-2">
@@ -37,20 +49,43 @@ const PostTweet = () => {
           placeholder="What is happening?!"
           className="bg-black text-xl placeholder:text-gray-600 outline-none"
         />
+        {(uploadedImageURL || isImageUploading) && (
+          <div className="h-20 w-20 relative">
+            {isImageUploading ? (
+              <div>
+                <LoadingSpinner/>
+              </div>
+            ) : (
+              <>
+                <div
+                  onClick={() => setUploadedImageURL(null)}
+                  className="absolute right-1 top-1 bg-black rounded-full bg-opacity-80"
+                >
+                  <CloseRoundedIcon />
+                </div>
+                <img
+                  className="object-cover w-full h-full rounded-xl"
+                  src={uploadedImageURL}
+                />
+              </>
+            )}
+          </div>
+        )}
         <div className="flex justify-between">
-          <label htmlFor="xy">
-            <BrokenImageOutlinedIcon sx={{color:"#3B82F6"}} />
+          <label htmlFor="fileInput">
+            <BrokenImageOutlinedIcon sx={{ color: "#3B82F6" }} />
           </label>
           <input
-            id="xy"
+            id="fileInput"
             className="hidden"
             type="file"
-            onChange={(e) => setFile(e.target.files[0])}
+            accept="image/jpeg, image/png, image/gif, image/webp"
+            onChange={handleImageUpload}
           />
           <button
             onClick={handlePost}
             className="bg-blue-500 rounded-full py-2 font-bold px-5 disabled:opacity-50"
-            disabled={!file && title == ""}
+            disabled={!uploadedImageURL && title == ""}
           >
             Post
           </button>

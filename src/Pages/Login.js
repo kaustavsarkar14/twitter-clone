2fc83ccec3ae8fa4c6877de175629f2730ae8884
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-21 16:05:15 +0530

    login page style

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,16 @@
 import { signInWithPopup } from "firebase/auth";
 import React from "react";
 import { auth, db, provider } from "../firebase";
-import { addDoc, collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
+import { APPLE_LOGO, GOOGLE_LOGO, LOGO_URL } from "../utils/constants";
 
 const Login = () => {
   const handleSignUpWithGoogle = async () => {
@@ -14,40 +23,64 @@ const Login = () => {
     try {
       const querySnapshot = await getDocs(q);
       if (!querySnapshot.empty) {
-        console.log(querySnapshot)
+        console.log(querySnapshot);
         querySnapshot.forEach((doc) => {
           console.log(doc.id, " => ", doc.data());
         });
       } else {
         console.log("No user found with the UID:", uid);
         const docRef = await addDoc(usersRef, {
-          name:result.user.displayName,
+          name: result.user.displayName,
           email: result.user.email,
-          photoURL:result.user.photoURL,
-          bannerURL:null,
-          bio:'',
+          photoURL: result.user.photoURL,
+          bannerURL: null,
+          bio: "",
           uid: uid,
         });
       }
     } catch (error) {
       console.error("Error fetching user:", error);
     }
-    try{
+    try {
       await addDoc(collection(db, "follow"), {
         userId: auth.currentUser.uid,
         followers: [],
         following: [],
       });
-    }
-    catch(err){
-
-    }
+    } catch (err) {}
   };
   return (
-    <div>
-      <button className="bg-blue-400" onClick={handleSignUpWithGoogle}>
-        Sign in With Google
-      </button>
+    <div className="flex flex-col md:flex-row h-[90vh] m-auto justify-center">
+      <div className="md:w-[55%] w-full flex md:justify-center justify-start p-4 md:mb-0 mb-10 items-center">
+        <img src={LOGO_URL} className="md:h-[50vh] h-[4rem]" alt="" />
+      </div>
+      <div className="md:w-[45%] w-full p-4 flex flex-col justify-center">
+        <h1 className="md:text-6xl text-5xl font-bold mb-12">Happening now</h1>
+        <h2 className="text-3xl font-bold my-6">Join today</h2>
+        <div className="flex flex-col gap-2 md:justify-start md:items-start justify-center items-center">
+          <button
+            className="bg-white text-black flex gap-2 rounded-full py-2 w-full md:w-[20rem] justify-center items-center"
+            onClick={handleSignUpWithGoogle}
+          >
+            <img src={GOOGLE_LOGO} className="h-6 w-6" alt="" />
+            <p className="font-semibold">Sign in With Google</p>
+          </button>
+          <button
+            className="bg-white text-black flex gap-2 rounded-full py-2 w-full md:w-[20rem] justify-center items-center"
+            onClick={handleSignUpWithGoogle}
+          >
+            <img src={APPLE_LOGO} className="w-5" alt="" />
+            <p className="font-bold">Sign in With Apple</p>
+          </button>
+          <h1 className="font-semibold pl-2 my-3">Already have and account?</h1>
+          <button
+            className="bg-blue-500 flex gap-2 rounded-full py-2 w-full md:w-[20rem] justify-center items-center"
+            onClick={handleSignUpWithGoogle}
+          >
+            <p className="font-bold">Sign in</p>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
 export const LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/5/57/X_logo_2023_%28white%29.png"
+export const GOOGLE_LOGO = "https://seeklogo.com/images/G/google-logo-28FA7991AF-seeklogo.com.png"
+export const APPLE_LOGO = "https://www.edigitalagency.com.au/wp-content/uploads/apple-logo-png-black-900x1106.png"

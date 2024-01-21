c242bd5e9e44322a2f1b9f25c7356eae63b53596
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-01-21 19:55:33 +0530

    env setup

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,16 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVDcpCteGZHpQty_bomohswifCTXjVsUE",
-  authDomain: "twitter-clone-a6c28.firebaseapp.com",
-  projectId: "twitter-clone-a6c28",
-  storageBucket: "twitter-clone-a6c28.appspot.com",
-  messagingSenderId: "637852263414",
-  appId: "1:637852263414:web:3f86502029574fbbdb9dd7",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const provider = new GoogleAuthProvider();
 export const auth = getAuth();
 export const db = getFirestore(app);
-export const storage = getStorage();
+export const storage = getStorage(app);
